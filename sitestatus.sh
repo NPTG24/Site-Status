a697af4e5f5ae0ebd3b8388dbc680de96f5f6e35
Felipe Canales Cayuqueo <64817454+NPTG24@users.noreply.github.com>
2022-08-26 05:21:26 -0400

    Update sitestatus.sh

--- sitestatus.sh
+++ sitestatus.sh
@@ -23,7 +23,7 @@ do
     	echo "Code: $code" 
     fi
     
-    echo -e "\n-------HttpsVerification-------"
+    echo -e "\n-------Https Verification-------"
     code=$(curl -s -o /dev/null -w "%{http_code}" https://$output)  
     if [ $code -eq 000 ] || [ $code -eq 400 ] || [ $code -eq 403 ] || [ $code -eq 404 ] || [ $code -eq 408 ]|| [ $code -eq 410 ]|| [ $code -eq 422 ]|| [ $code -eq 500 ] || [ $code -eq 502 ]|| [ $code -eq 503 ] || [ $code -eq 504 ]|| [ $code -eq 508 ]|| [ $code -eq 521 ]|| [ $code -eq 525 ]; then
     	echo "$output --> Site Down"
